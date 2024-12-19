29cd9cc31195155b095d0566efd941765310a0a6
Gaurav Karki <gaurav.karki2058@gmail.com>
2024-12-19 05:23:18 +0000

    feat: added random number generation and username input

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=game-t --no-align -c"
+
+RANDOM_VALUE= echo $((RANDOM % 1000 + 1 ))
+GUESS_COUNT=0
+
+echo "Enter your username:"
+read USERNAME
+
