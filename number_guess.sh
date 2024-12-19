304b32d4c522db64ea47fbd1bef620a0d066e7da
Gaurav Karki <gaurav.karki2058@gmail.com>
2024-12-19 05:32:00 +0000

    feat: added username checking

--- number_guess.sh
+++ number_guess.sh
@@ -8,3 +8,13 @@ GUESS_COUNT=0
 echo "Enter your username:"
 read USERNAME
 
+USER_EXISTS_RESULT=$($PSQL"select username,games_played,best_game from users where username = '$USERNAME';")
+
+if [[ -z $USER_EXISTS_RESULT ]]
+then
+  INSERT_NEW_USER=$($PSQL"insert into users(username) values('$USERNAME')")
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+else
+  IFS='|' read -r USERNAME GAMES_PLAYED BEST_GAME <<< $USER_EXISTS_RESULT
+  echo  "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
