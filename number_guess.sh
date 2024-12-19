13d9274b0149a4407518210f455c99e90cfc1a61
Gaurav Karki <gaurav.karki2058@gmail.com>
2024-12-19 05:35:21 +0000

    feat: added user number guess input

--- number_guess.sh
+++ number_guess.sh
@@ -4,6 +4,8 @@ PSQL="psql --username=freecodecamp --dbname=game-t --no-align -c"
 
 RANDOM_VALUE= echo $((RANDOM % 1000 + 1 ))
 GUESS_COUNT=0
+TOTAL_GAMES_PLAYED=0
+USER_BEST_GAME=0
 
 echo "Enter your username:"
 read USERNAME
@@ -16,5 +18,10 @@ then
   echo "Welcome, $USERNAME! It looks like this is your first time here."
 else
   IFS='|' read -r USERNAME GAMES_PLAYED BEST_GAME <<< $USER_EXISTS_RESULT
+  USER_BEST_GAME=$BEST_GAME
+  TOTAL_GAMES_PLAYED=$GAMES_PLAYED
   echo  "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
+
+echo "Guess the secret number between 1 and 1000:"
+read USER_GUESS
