d7824c6b8d26c092428eb810df7f1dfe6ab48e22
Gaurav Karki <gaurav.karki2058@gmail.com>
2024-12-19 05:57:20 +0000

    fix: fixed psql onnection variable.

--- number_guess.sh
+++ number_guess.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=game-t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=game -t --no-align -c"
 
-SECRET_NUMBER= echo $((RANDOM % 1000 + 1 ))
+SECRET_NUMBER=$((RANDOM % 1000 + 1))
 GUESS_COUNT=0
 TOTAL_GAMES_PLAYED=0
 USER_BEST_GAME=0
@@ -10,50 +10,49 @@ USER_BEST_GAME=0
 echo "Enter your username:"
 read USERNAME
 
-USER_EXISTS_RESULT=$($PSQL"select username,games_played,best_game from users where username = '$USERNAME';")
+USER_EXISTS_RESULT=$($PSQL "SELECT username, games_played, best_game FROM users WHERE username = '$USERNAME';")
 
 if [[ -z $USER_EXISTS_RESULT ]]
 then
-  INSERT_NEW_USER=$($PSQL"insert into users(username) values('$USERNAME')")
+  # New user
+  INSERT_NEW_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
   echo "Welcome, $USERNAME! It looks like this is your first time here."
 else
-  IFS='|' read -r USERNAME GAMES_PLAYED BEST_GAME <<< $USER_EXISTS_RESULT
+  # Existing user
+  IFS='|' read -r USERNAME GAMES_PLAYED BEST_GAME <<< "$USER_EXISTS_RESULT"
   USER_BEST_GAME=$BEST_GAME
   TOTAL_GAMES_PLAYED=$GAMES_PLAYED
-  echo  "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
-
 
 GUESS_GAME(){
-
-if [[ -z $1 ]]
-then
-  echo "Guess the secret number between 1 and 1000:"
-else
-  echo "$1"
-fi
-
-read USER_GUESS
-
-if [[ $USER_GUESS =~ ^[0-9]+$ ]]
-then
-  if [[ $USER_GUESS -gt $SECRET_NUMBER ]]
+  if [[ -z $1 ]]
   then
-    ((GUESS_COUNT++))
-    GUESS_GAME "It's lower than that, guess again:"
-  fi
-  elif [[ $USER_GUESS -gt $SECRET_NUMBER ]] 
-  then
-    ((GUESS_COUNT++))
-    GUESS_GAME "It's higher than that, guess again:"
-  fi
+    echo "Guess the secret number between 1 and 1000:"
   else
-  echo "You guessed it in $GUESS_COUNT tries. The secret number was $SECRET_NUMBER. Nice job!"
-else
-  echo "That is not an integer, guess again:"
-fi
+    echo "$1"
+  fi
 
+  read USER_GUESS
 
+  if [[ $USER_GUESS =~ ^[0-9]+$ ]]
+  then
+    if [[ $USER_GUESS -gt $SECRET_NUMBER ]]
+    then
+      ((GUESS_COUNT++))
+      GUESS_GAME "It's lower than that, guess again:"
+    elif [[ $USER_GUESS -lt $SECRET_NUMBER ]]
+    then
+      ((GUESS_COUNT++))
+      GUESS_GAME "It's higher than that, guess again:"
+    else
+      ((GUESS_COUNT++))
+      echo "You guessed it in $GUESS_COUNT tries. The secret number was $SECRET_NUMBER. Nice job!"
+    fi
+  else
+    echo "That is not an integer, guess again:"
+    GUESS_GAME
+  fi
 }
 
 GUESS_GAME
